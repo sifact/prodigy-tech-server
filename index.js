5b5b4b9391f35960a09e6edab67544850a26bae0
sifat <Sifatami596@gmail.com>
2022-10-26 00:17:28 +0600

    courses api added

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require("./data/courses.json");
+app.get("/", (req, res) => {
+    res.send("Course Project server running...");
+});
 
 app.listen(port, () => {
     console.log("Server running on port:", port);

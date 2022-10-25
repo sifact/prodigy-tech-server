6b3dcf34a5ced60b746f10ae9e8ba7fa24429509
sifat <Sifatami596@gmail.com>
2022-10-26 00:22:58 +0600

    id wise course api created

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.get("/", (req, res) => {
     res.send("Course Project server running...");
 });
 
+app.get("/courses", (req, res) => {
+    res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find((course) => course.id === id);
+    res.send(selectedCourse);
+});
+
 app.listen(port, () => {
     console.log("Server running on port:", port);
 });
